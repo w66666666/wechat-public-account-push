a9f71fb40fa172cc16b70659b4678e292bb5b801
w66666666 <114042846+w66666666@users.noreply.github.com>
2022-09-21 13:59:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -66,9 +66,9 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "hLnO0xMzURjMUknAUPH_TMGCZZUYXuS2X63OdBkkbzA",
       // 所在省份
-      province: "浙江",
+      province: "江西",
       // 所在城市
-      city: "嘉兴",
+      city: "九江",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '03-16',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -160,9 +160,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "江西",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "九江",
 
   /** 重要节日相关 */
 

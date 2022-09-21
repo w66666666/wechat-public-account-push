e2dcfbf33753fb58fe064d081d120bf0e1013203
w66666666 <114042846+w66666666@users.noreply.github.com>
2022-09-21 13:29:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,11 +64,11 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oNuEE6UoWj2paKmTho8J7qKTCLSk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "0GsehAF9IXlACznFl2pCXUENLu9aGJF17Ks4wU8P3AI",
+      useTemplateId: "hLnO0xMzURjMUknAUPH_TMGCZZUYXuS2X63OdBkkbzA",
       // 所在省份
       province: "浙江",
       // 所在城市
-      city: "海宁",
+      city: "嘉兴",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '03-16',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -78,7 +78,7 @@ export const config = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "2003", "date": "03-16"},
+        {"type": "*生日", "name": "婷婷宝贝", "year": "2003", "date": "03-16"},
         {"type": "节日", "name": "恋爱纪念日", "year": "2022", "date": "01-09"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "李伟东", "year": "2004", "date": "03-20"},
@@ -89,7 +89,7 @@ export const config = {
         // 在一起的日子
         {"keyword": "love_day", date: "2022-01-09"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-01-09"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
